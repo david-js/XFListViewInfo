ab620e5a925a8a05358aba8e5c2f6dca90c5fca4
David Sielaff <david@criticalhittech.com>
2017-12-07 11:28:36 -0800

    Add non-functional SearchBar that appears/disappears during ListView scrolling

--- ListInfoDemo/ListInfoDemo/MyListView.cs
+++ ListInfoDemo/ListInfoDemo/MyListView.cs
@@ -18,16 +18,42 @@ namespace ListInfoDemo
         public static readonly BindableProperty AtStartOfListProperty =
             BindableProperty.Create(nameof(AtStartOfList), typeof(bool), typeof(MyListView), false);
 
+        // Keep track of previous state and when the next time the reported value might change.
+        // _nextShowChange is stored to allow a delay before a change in the value is allowed to avoid a jittery look.
+        private bool? _lastShowValue;
+        private DateTime _nextShowChange;
+        public bool ShouldShowSearchBar
+        {
+            get {
+                var newValue = AtStartOfList || LastScrollDirection == ScrollToStart;
+                var shouldChange = (!_lastShowValue.HasValue) || (_lastShowValue.Value != newValue && _nextShowChange < DateTime.Now);
+
+                if (shouldChange)
+                {
+                    _lastShowValue = newValue;
+                    _nextShowChange = DateTime.Now.AddMilliseconds(250);
+                }
+
+                return shouldChange ? newValue : _lastShowValue.Value;
+            }
+        }
+
         public string LastScrollDirection
         {
             get { return (string)GetValue(LastScrollDirectionProperty); }
-            set { SetValue(LastScrollDirectionProperty, value); }
+            set {
+                SetValue(LastScrollDirectionProperty, value);
+                OnPropertyChanged(nameof(ShouldShowSearchBar));
+            }
         }
 
         public bool AtStartOfList
         {
             get { return (bool)GetValue(AtStartOfListProperty); }
-            set { SetValue(AtStartOfListProperty, value); }
+            set {
+                SetValue(AtStartOfListProperty, value);
+                OnPropertyChanged(nameof(ShouldShowSearchBar));
+            }
         }
     }
 }
