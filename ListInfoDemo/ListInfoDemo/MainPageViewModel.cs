80a58ce583c5db999734549015df0e504739357a
David Sielaff <david@criticalhittech.com>
2018-05-25 14:55:49 -0700

    Use view model rather than binding to the view

---
+++ ListInfoDemo/ListInfoDemo/MainPageViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListInfoDemo
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private bool _isAtStartOfList;
+        public bool IsAtStartOfList
+        {
+            get
+            {
+                return _isAtStartOfList;
+            }
+            set
+            {
+                if (value != _isAtStartOfList)
+                {
+                    _isAtStartOfList = value;
+                    NotifyChanged();
+                    NotifyChanged(nameof(ShouldShowSearchBar));
+                }
+            }
+        }
+
+        private string _lastScrollDirection;
+        public string ScrollingDirection
+        {
+            get
+            {
+                return _lastScrollDirection;
+            }
+            set
+            {
+                if (value != _lastScrollDirection)
+                {
+                    _lastScrollDirection = value;
+                    NotifyChanged();
+                    NotifyChanged(nameof(ShouldShowSearchBar));
+                }
+            }
+        }
+
+        // Keep track of previous state and when the next time the reported value might change.
+        // _nextShowChange is stored to allow a delay before a change in the value is allowed to avoid a jittery look.
+        private bool? _lastShowValue;
+        private DateTime _nextShowChange;
+        public bool ShouldShowSearchBar
+        {
+            get
+            {
+                var newValue = IsAtStartOfList || ScrollingDirection == MyListView.ScrollToStart;
+                var shouldChange = (!_lastShowValue.HasValue) || (_lastShowValue.Value != newValue && _nextShowChange < DateTime.Now);
+
+                if (shouldChange)
+                {
+                    _lastShowValue = newValue;
+                    _nextShowChange = DateTime.Now.AddMilliseconds(250);
+                }
+
+                return shouldChange ? newValue : _lastShowValue.Value;
+            }
+        }
+
+        public List<MovieRating> Movies
+        {
+            get
+            {
+                return MovieRatingData.Movies;
+            }
+        }
+    }
+}
---
+++ ListInfoDemo/ListInfoDemo/MovieRatingData.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListInfoDemo
+{
+    public class MovieRating
+    {
+        public string Name { get; set; }
+        public int Rating { get; set; }
+    }
+
+    class MovieRatingData
+    {
+        public static List<MovieRating> Movies { get; set; } =
+            new List<MovieRating>()
+            {
+                new MovieRating() { Name = "Star Wars, Ep 1", Rating = 3 },
+                new MovieRating() { Name = "Star Wars, Ep 2", Rating = 2 },
+                new MovieRating() { Name = "Star Wars, Ep 3", Rating = 1 },
+                new MovieRating() { Name = "Star Wars, Ep 4", Rating = 5 },
+                new MovieRating() { Name = "Star Wars, Ep 5", Rating = 5 },
+                new MovieRating() { Name = "Star Wars, Ep 6", Rating = 5 },
+                new MovieRating() { Name = "Star Trek: The Motion Picture", Rating = 3 },
+                new MovieRating() { Name = "Star Trek II: The Wrath of Khan", Rating = 4 },
+                new MovieRating() { Name = "Star Trek III: The Search for Spock", Rating = 3 },
+                new MovieRating() { Name = "Star Trek IV: The Voyage Home", Rating = 5 },
+                new MovieRating() { Name = "Star Trek V: The Final Frontier", Rating = 3 },
+                new MovieRating() { Name = "Star Trek VI: The Undiscovered Country", Rating = 3 },
+                new MovieRating() { Name = "Star Trek: Generations", Rating = 4 },
+                new MovieRating() { Name = "Star Trek (2009)", Rating = 2 },
+                new MovieRating() { Name = "Star Trek Into Darkness", Rating = 2 },
+                new MovieRating() { Name = "Star Wars, Ep 1", Rating = 3 },
+                new MovieRating() { Name = "Star Wars, Ep 2", Rating = 2 },
+                new MovieRating() { Name = "Star Wars, Ep 3", Rating = 1 },
+                new MovieRating() { Name = "Star Wars, Ep 4", Rating = 5 },
+                new MovieRating() { Name = "Star Wars, Ep 5", Rating = 5 },
+                new MovieRating() { Name = "Star Wars, Ep 6", Rating = 5 },
+                new MovieRating() { Name = "Star Trek: The Motion Picture", Rating = 3 },
+                new MovieRating() { Name = "Star Trek II: The Wrath of Khan", Rating = 4 },
+                new MovieRating() { Name = "Star Trek III: The Search for Spock", Rating = 3 },
+                new MovieRating() { Name = "Star Trek IV: The Voyage Home", Rating = 5 },
+                new MovieRating() { Name = "Star Trek V: The Final Frontier", Rating = 3 },
+                new MovieRating() { Name = "Star Trek VI: The Undiscovered Country", Rating = 3 },
+                new MovieRating() { Name = "Star Trek: Generations", Rating = 4 },
+                new MovieRating() { Name = "Star Trek (2009)", Rating = 2 },
+                new MovieRating() { Name = "Star Trek Into Darkness", Rating = 2 },
+                new MovieRating() { Name = "Star Wars, Ep 1", Rating = 3 },
+                new MovieRating() { Name = "Star Wars, Ep 2", Rating = 2 },
+                new MovieRating() { Name = "Star Wars, Ep 3", Rating = 1 },
+                new MovieRating() { Name = "Star Wars, Ep 4", Rating = 5 },
+                new MovieRating() { Name = "Star Wars, Ep 5", Rating = 5 },
+                new MovieRating() { Name = "Star Wars, Ep 6", Rating = 5 },
+                new MovieRating() { Name = "Star Trek: The Motion Picture", Rating = 3 },
+                new MovieRating() { Name = "Star Trek II: The Wrath of Khan", Rating = 4 },
+                new MovieRating() { Name = "Star Trek III: The Search for Spock", Rating = 3 },
+                new MovieRating() { Name = "Star Trek IV: The Voyage Home", Rating = 5 },
+                new MovieRating() { Name = "Star Trek V: The Final Frontier", Rating = 3 },
+                new MovieRating() { Name = "Star Trek VI: The Undiscovered Country", Rating = 3 },
+                new MovieRating() { Name = "Star Trek: Generations", Rating = 4 },
+                new MovieRating() { Name = "Star Trek (2009)", Rating = 2 },
+                new MovieRating() { Name = "Star Trek Into Darkness", Rating = 2 },
+            };
+    }
+}
